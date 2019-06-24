476ef2ab3f26ae63ca43a932f93a015e551e9b0d
tonimk <51784705+tonimk@users.noreply.github.com>
2019-06-24 14:08:29 +0300

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,1 +1,18 @@
-import antigravity
+i = 0
+while i < 3:
+    nimi = input("Anna nimesi: ")
+    print("Sinä olet " + nimi)
+    ikä = int(input("Anna ikäsi: "))
+    print("Ikäsi on:",ikä)
+    kaverikä = int(input("Kuinka vanha mielikuvituskaverisi on? "))
+    if ikä > kaverikä:
+        print("Sinä olet vanhempi")
+    elif ikä < kaverikä:
+        print("Kaveri on vanhempi")
+    else:
+        print("Olette yhtä vanhoja")
+    i = i + 1
+    if i < 3:
+        print("Tämä käydään vielä", 3 - i,"kertaa")
+    else:
+        print("Loppu!")
